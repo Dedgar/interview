b6f2899ce3a9aa3c0bcbab2db2b8c9068ef06e08
dedgar <dedgar@shinobu.ninja>
2020-03-10 11:48:16 -0700

    adding comments

--- array_rotate/array_rotate.go
+++ array_rotate/array_rotate.go
@@ -9,14 +9,23 @@ func rotate(nums []int, k int) {
 		fmt.Println(nums)
 		return
 	}
+
+	// get the modulus since we only need to move by the remainder.
+	// it saves us from having to do math around where the numbers should be
+	// after shifting an array of items a hundred times.
 	mod := aLen - (k % aLen)
 	fmt.Println("aLen, k, mod", aLen, k, mod)
 
+	// start making new arrays with everything before the shift number
+	// and everything after the shift number
 	begin := nums[mod:]
 	end := nums[:mod]
 
+	// make a new array with the items shifted by the correct amount
 	begin = append(begin, end...)
 
+	// copy because of the weird challenge restrictions, or else we could just
+	// return the begin var at this point.
 	copy(nums, begin)
 
 	fmt.Println(nums)
