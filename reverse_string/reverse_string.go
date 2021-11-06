5f7c2bde2a3540e0f203ccabe3a246fe3811448a
dedgar <dedgar@shinobu.ninja>
2021-11-05 21:02:21 -0700

    adding another solution to reverse_string. 32ms beats 92.81%. 6.7MB less than 87%

--- reverse_string/reverse_string.go
+++ reverse_string/reverse_string.go
@@ -33,8 +33,36 @@ func reverseString(s []byte) {
 	fmt.Println(string(s[:]))
 }
 
+// a slightly cleaner version that only swaps values if they're different.
+// reduces the total number of write operations for arrays that have lots of similar values.
+func reverseString2(s []byte) {
+	l1 := len(s)
+
+	// bail if we don't need to reverse the array.
+	if l1 < 2 {
+		fmt.Println(string(s))
+		return
+	}
+
+	// keep some reference numbers of where we're at in the array,
+	// as we work from both ends toward the middle.
+	bot := 0
+	top := l1 - 1
+
+	for bot < top {
+		// swap the current items if they aren't the same already
+		if s[bot] != s[top] {
+			s[bot], s[top] = s[top], s[bot]
+		}
+
+		bot++
+		top--
+	}
+	fmt.Println(string(s[:]))
+}
+
 func main() {
-	reverseString([]byte{'s', 'o', 'm', 'e', 's', 't', 'r', 'i', 'n', 'g', 't', 'o', 'r', 'e', 'v', 'e', 'r', 's', 'e'})
-	reverseString([]byte{'s', 'o', 'u', 'p'})
-	reverseString([]byte{'s'})
+	reverseString2([]byte{'s', 'o', 'm', 'e', 's', 't', 'r', 'i', 'n', 'g', 't', 'o', 'r', 'e', 'v', 'e', 'r', 's', 'e'})
+	reverseString2([]byte{'s', 'o', 'u', 'p'})
+	reverseString2([]byte{'s'})
 }
